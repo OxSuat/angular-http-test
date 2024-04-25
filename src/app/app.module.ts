c877d85477e2bb206d10f7c9fdc0c0893dae9cf6
OxSuat <suat.izmir@doctolib.com>
2024-04-25 15:31:56 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataViewerComponent } from './data-viewer/data-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataViewerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
